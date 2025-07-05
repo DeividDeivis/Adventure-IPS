57a0b7d147fa1b92646051bc76ed14e4acbee3fb
David Ruiz <deivid.ruiz93@gmail.com>
2025-07-05 11:06:45 -0300

    UI prefab update + Life bar + Start Button + Fuego Assets

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,19 +7,28 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     [SerializeField] private List<GameObject> lifes = new List<GameObject>();
+    [SerializeField] private Slider lifeBar;
+    private int lifesInt = 3;
     [SerializeField] private TextMeshProUGUI coinsTxt;
+    [SerializeField] private Button startGame;
 
     [SerializeField] private GameObject loseScreen;
 
     private void Start()
     {
         PlayerController.OnPlayerHit += SubstractLife;
+        lifeBar.maxValue = lifesInt;
+        lifeBar.value = lifesInt;
+
+        //startGame.onClick.AddListener(StartClick);
     }
 
     public void SubstractLife() 
     {
         GameObject _life = lifes.First(life => life.activeSelf.Equals(true));
         _life.SetActive(false);
+        lifesInt--;
+        lifeBar.value = lifesInt;
     }
 
     public void AddCoins(int amount) { coinsTxt.text = $"COINS: {amount}"; }
@@ -27,5 +36,10 @@ public class UIController : MonoBehaviour
     public void ShowLoseScreen() 
     {
         loseScreen.SetActive(true);
+    }
+
+    public void StartClick() 
+    {
+        Debug.Log("START GAME CLICKED");
     }
 }
